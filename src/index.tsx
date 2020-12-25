dd5f6ad85c4e1b57cef451d10a22c3bbe5d2951b
alicereactus <agamia@mail.ru>
2020-12-26 01:58:37 +0300

    add counter two with redux, refactor Button by creating component

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import AppWithRedux from './AppWithRedux';
+import { Provider } from 'react-redux';
+import { store } from './redux/state';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppWithRedux />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
